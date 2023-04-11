dc25928d20e4e0b3e56af39bb66b61aff9621fe8
Luis Miguel <lmavendano97@outlook.es>
2023-04-10 19:01:21 -0500

    next commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,12 +2,13 @@ const User = require('../models/user.model');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, role } = req.body;
 
   const user = await User.create({
     name,
     email,
     password,
+    role,
   });
 
   res.status(201).json({
--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,11 +1,50 @@
+const AppError = require('../utils/appError');
+
+const handleCastError22P02 = () =>
+  new AppError('Some type of data send does not match was expected', 400);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //Programming or other unknown error: don't leak error details
+    console.error('Error 🧨', err);
+    res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'fail';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    let error = err;
+    console.log(error.parent?.code);
+
+    if (error.parent?.code === '22P02') error = handleCastError22P02();
+
+    sendErrorProd(error, res);
+  }
 };
 
 module.exports = globalErrorHandler;
